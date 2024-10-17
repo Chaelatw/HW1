48ff0bc73f301c1b20c1b4766576a6b14fd5399f
Chaela <chaelawills55@gmail.com>
2024-10-17 10:20:26 -0400

    homework

--- src/main/java/homework/VariableScope.java
+++ src/main/java/homework/VariableScope.java
@@ -1,4 +1,6 @@
 package homework;
 
 public class VariableScope {
+    //How do we get rid of an object? - we set it to null
+    //A constructor with parameters is called a parameterized constructor
 }
